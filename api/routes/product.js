7f9f3f1d2b67933393aa3c06a0c6b6c207e32d07
Adesola Busari <adesolaisa3@gmail.com>
2025-05-13 08:13:42 +0100

    configured database

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,52 +1,96 @@
-const express = require('express')
-const route = express.Router()
+const express = require('express');
+const route = express.Router();
+const mongoose = require("mongoose")
+const productModel = require("../models/product");
 
 
+// get all products
 route.get('/',(req, res, next)=>{
-    
-    res.status(200).json({
-        message: "Handling GET requuest to /products"
+    productModel.find()
+    .exec()
+    .then(docs=>{
+        console.log(docs);
+        res.status(200).json(docs);
+
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json(docs);
     });
+    
 });
 
+
+// create a new product 
 route.post('/',(req, res, next)=>{
-    const product ={
-        name:req.body.name,
+    const product = new productModel( {
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
         price: req.body.price
-    };
+    })
+    product.save().then(result=>{
+        console.log(result);
+    })
+    .catch(err => console.log(err))
     res.status(200).json({
-        message: "Handling POST requuest to /products",
+        message: "Handling POST request to /products",
         createdProduct: product
 
     });
 });
 
+
+// get by product by ID
 route.get('/:productId',(req, res, next)=>{
     const id = req.params.productId;
-    if (id==='special'){
-        res.status(200).json({
-            message: "You discovered  the special ID",
-            id: id
-        });
-        
-    }
-    else{
-        res.status(200).json({
-            message: "you passed an id"
-        });
+    productModel.findById(id)
+    .exec()
+    .then(doc =>{
+        console.log(doc);
+        // if (doc.lenght >= 0){
+            res.status(200).json({doc})
+        // }else{
+            // res.status(404).json({
+                // message: "No Entry found"
+            // })
+        // }
+    })
+    .catch(err=> {console.log(err)});
 
-    }
 });
 
+
+// edit product by ID
 route.patch('/:productId', (req,res,next)=>{
-    res.status(200).json({
-        message:"Updated product !"
-    });
+    const id = req.params.productId
+    const updateOps = {};
+    for (const ops  of req.body){
+        updateOps[ops.propName]= ops.value
+    }
+    productModel.update({_id:id},{$set: updateOps}).exec()
+    .then(result =>{
+        res.status(200).json(result)
+    })
+    .catch(err=> {
+        console.log(err);
+        res.status(500).json({error:err});
+    })
+    
 });
 
+// delete Product by id
 route.delete('/:productId', (req,res,next)=>{
-    res.status(200).json({
-        message:"Deleted product !"
-    });
+    const id = req.params.productId
+    productModel.deleteOne({_id:id})
+    .exec()
+    .then(result=>{
+        res.status(200).json(result);
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error:err
+        });
+    })
 });
 module.exports= route
--- app.js
+++ app.js
@@ -2,13 +2,35 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose")
 
 const productRoutes =  require('./api/routes/product')
 const OrderRoutes = require("./api/routes/order")
 
+
+mongoose.connect(
+    "mongodb+srv://busade:"+process.env.MONGO_ATLAS_PW +
+    "@cluster0.xwb0u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    
+).then(()=> console.log("MongoDB connected")) 
+.catch(err=> console.logg("MongoDB connection error: ", err));
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+app.use ((req,res,next) =>{
+    res.header("Access-Control-Allow-Origin",'*');
+    res.header(
+        "Access-Control-Headers",
+        "Origin, X-Requested-with, Content-Type, Accept,Authorization"  
+    );
+    if (req.method ==="OPTIONS"){
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, GET, DELETE")
+        return status(200).json({});
+    }
+    next();
+});
 
 app.use('/products',productRoutes);
 app.use('/order', OrderRoutes);
